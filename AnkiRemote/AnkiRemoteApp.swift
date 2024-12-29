46e6d0157701f9a9375ea79e92c30e0bb897490b
dmunagapati <dmunagapati@gmail.com>
2024-12-29 01:48:18 -0500

    Initial Commit

---
+++ AnkiRemote/AnkiRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnkiRemoteApp.swift
+//  AnkiRemote
+//
+//  Created by Deepthi Munagapati on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct AnkiRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnkiRemoteTests/AnkiRemoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnkiRemoteTests.swift
+//  AnkiRemoteTests
+//
+//  Created by Deepthi Munagapati on 12/29/24.
+//
+
+import Testing
+@testable import AnkiRemote
+
+struct AnkiRemoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
